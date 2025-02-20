12bc6a4782592bccb1b65db4fdb58185fba3421c
Funwi Che <funwiche.dev@gmail.com>
2025-02-20 21:55:11 +0100

    sss

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,7 @@
 window.onload = () => {
+  const form = document.querySelector("#contact-form");
+  const _success = document.querySelector("#error-success");
+  const _error = document.querySelector("#error-alert");
   const allItems = document.querySelectorAll(".grid-item");
   allItems.forEach((item) => {
     const rowHeight = 10; // Same as grid-auto-rows
@@ -8,5 +11,32 @@ window.onload = () => {
       .getBoundingClientRect().height;
     const rowSpan = Math.ceil((contentHeight + rowGap) / (rowHeight + rowGap));
     item.style.gridRowEnd = `span ${rowSpan}`;
+  });
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const url = "http://localhost:3000/mailer";
+    // const url = "https://xtremnet.vercel.app/mailer";
+
+    const fd = new FormData(e.target);
+    const data = {
+      to: "geral@jlmpanel.com",
+    };
+    fd.forEach((value, key) => {
+      data[key] = value;
+    });
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .catch((res) => {
+        if (res.ok) alert("Message sent successfully!");
+        else alert("An error occured. Please try again!");
+      })
+      .catch(() => {
+        alert("An error occured. Please try again!");
+      });
+
+    console.log(data);
   });
 };
