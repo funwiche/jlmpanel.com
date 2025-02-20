208082e770e78cba15b82293976c027303568700
Funwi Che <funwiche.dev@gmail.com>
2025-02-20 23:27:00 +0100

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,38 +10,6 @@ window.onload = () => {
     item.style.gridRowEnd = `span ${rowSpan}`;
   });
   document
-    .getElementById("subscription_form")
-    .addEventListener("submit", function (e) {
-      e.preventDefault(); // Prevent page reload
-      const error = document.querySelector("#error-alert");
-      const success = document.querySelector("#success-alert");
-
-      const to = "geral@jlmpanel.com";
-      const url = "https://xtremnet.vercel.app/mailer";
-
-      const formData = new FormData(this);
-
-      // Convert FormData to JSON
-      const data = Object.fromEntries(formData.entries());
-      console.log(data);
-      // fetch(url, {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   body: JSON.stringify({ to, ...data }),
-      // })
-      //   .then((res) => {
-      //     if (res.ok) success.classList.remove("hidden");
-      //     else error.classList.remove("hidden");
-      //   })
-      //   .catch(() => error.classList.remove("hidden"))
-      //   .finally(() => {
-      //     setTimeout(() => {
-      //       error.classList.add("hidden");
-      //       success.classList.add("hidden");
-      //     }, 6000);
-      //   });
-    });
-  document
     .getElementById("contact-form")
     .addEventListener("submit", function (e) {
       e.preventDefault(); // Prevent page reload
